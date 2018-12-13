369630104c365ab64e1e6344205657aa6da55c78
alice <xaliceli@gmail.com>
2018-12-13 17:44:30 -0600

    np->surface conversion, rand controls

--- eternal-face/visualize.py
+++ eternal-face/visualize.py
@@ -15,13 +15,29 @@ import numpy as np
 import pygame
 
 
-def set_squares(base, offset_x, offset_y, images):
+def np_to_surface(array):
+    new_array = array.copy()
+    new_array[:, :, 0] = array[:, :, 2]
+    new_array[:, :, 2] = array[:, :, 0]
+    surface = pygame.surfarray.make_surface(np.flip(np.rot90(np.uint8(new_array)), 0))
+    return surface
+
+def set_squares(base, offsets, images, prev=None, pct_blank=0):
     dim = images[0].shape[0]
-    base[offset_y:offset_y + dim, offset_x:offset_x + dim] = images[0]
-    base[offset_y/4:offset_y/4 + dim, offset_x/4:offset_x/4 + dim] = images[1]
-    base[offset_y/4:offset_y/4 + dim, -(dim + offset_x/4):-offset_x/4] = images[2]
-    base[-(dim + offset_y/4):-offset_y/4, offset_x/4:offset_x/4 + dim] = images[3]
-    base[-(dim + offset_y/4):-offset_y/4, -(dim + offset_x/4):-offset_x/4] = images[4]
+    bounds = [(offsets[1], offsets[1] + dim, offsets[0], offsets[0] + dim),
+              (offsets[1]/4, offsets[1]/4 + dim, offsets[0]/4, offsets[0]/4 + dim),
+              (offsets[1]/4, offsets[1]/4 + dim, -(dim + offsets[0]/4), -offsets[0]/4),
+              (-(dim + offsets[1]/4), -offsets[1]/4, offsets[0]/4, offsets[0]/4 + dim),
+              (-(dim + offsets[1]/4), -offsets[1]/4, -(dim + offsets[0]/4), -offsets[0]/4)]
+    if random.random() < pct_blank:
+        idx = random.randint(0, len(images) - 1)
+        if np.sum(prev) is None:
+            images[idx] = np.zeros(images[0].shape)
+        else:
+            images[idx] = prev[bounds[idx][0]:bounds[idx][1], bounds[idx][2]:bounds[idx][3]]
+
+    for loc, square in enumerate(images):
+        base[bounds[loc][0]:bounds[loc][1], bounds[loc][2]:bounds[loc][3]] = square
 
     return base
 
@@ -65,7 +81,7 @@ def resize_warps(warps, dim):
 
 def animation(warps, texture, target_dim, obj_dim, w_size):
 
-    output = np.empty(target_dim)
+    output = np.zeros(target_dim)
     offset_y = (target_dim[0] - obj_dim)/2
     offset_x = (target_dim[1] - obj_dim)/2
     if offset_y < 0:
@@ -73,23 +89,19 @@ def animation(warps, texture, target_dim, obj_dim, w_size):
     if offset_x < 0:
         offset_x = 0
 
-    # frames = progress_texture(np.empty(target_dim), w_size, texture)
-
     pygame.init()
     screen = pygame.display.set_mode((target_dim[1], target_dim[0]))
     pygame.mouse.set_visible(0)
 
     phase = 1
 
-    new = set_squares(output, offset_x, offset_y, random.sample(warps, 5))
-    new_array = new.copy()
-    new_array[:, :, 0] = new[:, :, 2]
-    new_array[:, :, 2] = new[:, :, 0]
-    show = pygame.surfarray.make_surface(np.rot90(np.uint8(output)))
+    new = set_squares(output, (offset_x, offset_y), random.sample(warps, 5))
+    show = np_to_surface(new)
 
     timer = pygame.USEREVENT + 1
-    timer_value = 500
+    timer_value = 1000
     pygame.time.set_timer(timer, timer_value)
+    rand_factor = 1
     idx = -1
 
     while True:
@@ -102,20 +114,24 @@ def animation(warps, texture, target_dim, obj_dim, w_size):
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 phase += 1
             if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                timer_value = 2 * timer_value
+                timer_value = int(1.1 * timer_value)
                 pygame.time.set_timer(timer, timer_value)
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                timer_value = .5 * timer_value
+                timer_value = int(.9 * timer_value)
                 pygame.time.set_timer(timer, timer_value)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                if rand_factor >= .1:
+                    rand_factor -= .1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                if rand_factor <= .9:
+                    rand_factor += .1
 
         if phase == 1:
             screen.blit(show, (0, 0))
             if pygame.event.get(timer):
-                new = set_squares(output, offset_x, offset_y, random.sample(warps, 5))
-                new_array = new.copy()
-                new_array[:, :, 0] = new[:, :, 2]
-                new_array[:, :, 2] = new[:, :, 0]
-                show = pygame.surfarray.make_surface(np.rot90(np.uint8(new_array)))
+                new = set_squares(output, (offset_x, offset_y), random.sample(warps, 5),
+                                  None, rand_factor)
+                show = np_to_surface(new)
                 pygame.display.update()
 
         if phase == 2:
@@ -125,21 +141,18 @@ def animation(warps, texture, target_dim, obj_dim, w_size):
                 h_range = output.shape[1] / w_size + 1
                 v_block, h_block = idx / h_range, idx % h_range
                 texture_frame = add_texture(output, v_block, h_block, w_size, texture)
-                new = set_squares(texture_frame, offset_x, offset_y, random.sample(warps, 5))
-                new_array = new.copy()
-                new_array[:, :, 0] = new[:, :, 2]
-                new_array[:, :, 2] = new[:, :, 0]
-                show = pygame.surfarray.make_surface(np.flip(np.rot90(np.uint8(new_array)), 0))
+                new = set_squares(texture_frame, (offset_x, offset_y), random.sample(warps, 5),
+                                  new.copy(), rand_factor)
+                show = np_to_surface(new)
                 pygame.display.update()
 
         if phase == 3:
+            rand_factor = 0
             screen.blit(show, (0, 0))
             if pygame.event.get(timer):
-                new = set_squares(output.copy(), offset_x, offset_y, random.sample(warps, 5))
-                new_array = new.copy()
-                new_array[:, :, 0] = new[:, :, 2]
-                new_array[:, :, 2] = new[:, :, 0]
-                show = pygame.surfarray.make_surface(np.rot90(np.uint8(new_array)))
+                new = set_squares(np.zeros(target_dim), (offset_x, offset_y), random.sample(warps, 5),
+                                  None, rand_factor)
+                show = np_to_surface(new)
                 pygame.display.update()
 
 
